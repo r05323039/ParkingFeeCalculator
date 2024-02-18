69222dda04238ed6858637242115467facbf1291
ian.lin@webcomm.com.tw <ian.lin@webcomm.com.tw>
2024-02-18 20:38:37 +0800

    refactor test

--- src/test/java/ian/parkingfeecalculator/service/ParkingFeeCalculatorTest.java
+++ src/test/java/ian/parkingfeecalculator/service/ParkingFeeCalculatorTest.java
@@ -1,8 +1,8 @@
 package ian.parkingfeecalculator.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,51 +14,68 @@ import static org.junit.jupiter.api.Assertions.*;
  * 當日上限150 / 2400 (隔日另計)
  */
 class ParkingFeeCalculatorTest {
-    private ParkingFeeCalculator sut = new ParkingFeeCalculator();
+    private ParkingFeeCalculator sut;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private long actual;
+
+    @BeforeEach
+    void setUp() {
+        sut = new ParkingFeeCalculator();
+    }
+
+    private void assert_fee_is(int expected) {
+        assertEquals(expected, actual);
+    }
+
+    private void given_parking_end(String endText) {
+        end = LocalDateTime.parse(endText);
+    }
+
+    private void given_parking_start(String startText) {
+        start = LocalDateTime.parse(startText);
+    }
+
+    private void calculate_fee() {
+        actual = sut.getFee(start, end);
+    }
 
     @Test
     void below_15_mins_free() {
-        LocalDateTime start = LocalDateTime.parse("2024-01-01T00:00:00");
-        LocalDateTime end = LocalDateTime.parse("2024-01-01T00:14:59");
-
-        long actual = sut.getFee(start, end);
-
-        assertEquals(0, actual);
+        given_parking_start("2024-01-01T00:00:00");
+        given_parking_end("2024-01-01T00:14:59");
+        calculate_fee();
+        assert_fee_is(0);
     }
 
     @Test
     void _15_mins_fee_30() {
-        LocalDateTime start = LocalDateTime.parse("2024-01-01T00:00:00");
-        LocalDateTime end = LocalDateTime.parse("2024-01-01T00:15:00");
-        long actual = sut.getFee(start, end);
-
-        assertEquals(30, actual);
+        given_parking_start("2024-01-01T00:00:00");
+        given_parking_end("2024-01-01T00:15:00");
+        calculate_fee();
+        assert_fee_is(30);
     }
     @Test
     void _30_mins_fee_60() {
-        LocalDateTime start = LocalDateTime.parse("2024-01-01T00:00:00");
-        LocalDateTime end = LocalDateTime.parse("2024-01-01T00:30:00");
-        long actual = sut.getFee(start, end);
-
-        assertEquals(60, actual);
+        given_parking_start("2024-01-01T00:00:00");
+        given_parking_end("2024-01-01T00:30:00");
+        calculate_fee();
+        assert_fee_is(60);
     }
 
     @Test
     void _60_mins_fee_90() {
-        LocalDateTime start = LocalDateTime.parse("2024-01-01T00:00:00");
-        LocalDateTime end = LocalDateTime.parse("2024-01-01T01:00:00");
-        long actual = sut.getFee(start, end);
-
-        assertEquals(90, actual);
+        given_parking_start("2024-01-01T00:00:00");
+        given_parking_end("2024-01-01T01:00:00");
+        calculate_fee();
+        assert_fee_is(90);
     }
 
     @Test
     void regular_ceiling_fee_150() {
-        LocalDateTime start = LocalDateTime.parse("2024-01-01T00:00:00");
-        LocalDateTime end = LocalDateTime.parse("2024-01-01T23:59:59");
-        long actual = sut.getFee(start, end);
-
-        assertEquals(150, actual);
-
+        given_parking_start("2024-01-01T00:00:00");
+        given_parking_end("2024-01-01T23:59:59");
+        calculate_fee();
+        assert_fee_is(150);
     }
 }
