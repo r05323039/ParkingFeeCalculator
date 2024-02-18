0b5d72f37d1940db1aef67b15b0826990e0482cf
ian.lin@webcomm.com.tw <ian.lin@webcomm.com.tw>
2024-02-18 14:50:12 +0800

    開介面

--- src/main/java/ian/parkingfeecalculator/service/ParkingFeeCalculator.java
+++ src/main/java/ian/parkingfeecalculator/service/ParkingFeeCalculator.java
@@ -1,4 +1,13 @@
 package ian.parkingfeecalculator.service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class ParkingFeeCalculator {
+    public long getFee(LocalDateTime start, LocalDateTime end) {
+        Duration duration = Duration.between(start, end);
+        if (duration.minusMinutes(15).isNegative())
+            return 0;
+        return 30;
+    }
 }
--- src/test/java/ian/parkingfeecalculator/service/ParkingFeeCalculatorTest.java
+++ src/test/java/ian/parkingfeecalculator/service/ParkingFeeCalculatorTest.java
@@ -1,13 +1,36 @@
 package ian.parkingfeecalculator.service;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * 15分內免費
  * 平日 / 假日與國定假日
- *  每小時60 / 100 (以半小時計)
- *  當日上限150 / 2400 (隔日另計)
+ * 每小時60 / 100 (以半小時計)
+ * 當日上限150 / 2400 (隔日另計)
  */
 class ParkingFeeCalculatorTest {
+    private ParkingFeeCalculator sut = new ParkingFeeCalculator();
 
+    @Test
+    void below_15_mins_free() {
+        LocalDateTime start = LocalDateTime.of(2024,1,1,0,0,0);
+        LocalDateTime end = LocalDateTime.of(2024,1,1,0,14,59);
+
+        long actual = sut.getFee(start, end);
+
+        assertEquals(0, actual);
+    }
+    @Test
+    void _15_mins_fee_30() {
+        LocalDateTime start = LocalDateTime.of(2024,1,1,0,0,0);
+        LocalDateTime end = LocalDateTime.of(2024,1,1,0,15,0);
+
+        long actual = sut.getFee(start, end);
+
+        assertEquals(30, actual);
+    }
 }
