6827ee4325ded14193f8c78727af027926c40f86
ian.lin <ian.lin@webcomm.com.tw>
2024-02-29 14:45:14 +0800

    refactor

--- src/main/java/ian/parkingfeecalculator/service/Calendar.java
+++ src/main/java/ian/parkingfeecalculator/service/Calendar.java
@@ -7,14 +7,14 @@ import java.util.List;
 import java.util.Set;
 
 public class Calendar {
+    private Set<LocalDate> nationalHolidays = new HashSet<>();
 
-
-    boolean isHoliday(LocalDate today) {
-
-        Set<LocalDate> nationalHolidays = new HashSet<>();
+    public Calendar() {
         nationalHolidays.add(LocalDate.parse("2024-01-01"));
         nationalHolidays.add(LocalDate.parse("2024-02-28"));
+    }
 
+    boolean isHoliday(LocalDate today) {
         boolean isNationalHolidays = nationalHolidays.contains(today);
         boolean isWeekend = List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(today.getDayOfWeek());
         return isWeekend || isNationalHolidays;
