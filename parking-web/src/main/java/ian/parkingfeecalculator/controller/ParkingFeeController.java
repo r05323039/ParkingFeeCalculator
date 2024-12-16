dfc963728af724df9dabe0cec0f2986eb7e8f5aa
ian.lin@webcomm.com.tw <ian.lin@webcomm.com.tw>
2024-12-16 17:52:37 +0800

    API hello

--- parking-web/src/main/java/ian/parkingfeecalculator/controller/ParkingFeeController.java
+++ parking-web/src/main/java/ian/parkingfeecalculator/controller/ParkingFeeController.java
@@ -4,8 +4,10 @@ import ian.parkingfeecalculator.service.ParkingService;
 import ian.parkingfeecalculator.vo.ParkingRequestVo;
 import ian.parkingfeecalculator.vo.ParkingResponseVo;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,10 @@ public class ParkingFeeController {
     public ResponseEntity<ParkingResponseVo> endParking(@Valid @RequestBody ParkingRequestVo request) {
         return ResponseEntity.ok()
                 .body(parkingService.endParking(request));
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
     }
 }
